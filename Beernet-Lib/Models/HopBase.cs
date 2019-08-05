57aa5cec8c76d0ce272d6ec03d464ad8f08695fd
nick <nickschwartzrox@gmail.com>
2019-08-04 21:21:06 -0500

    Added the adorable beginnings of the ingredient manager.

--- Beernet-Lib/Models/HopBase.cs
+++ Beernet-Lib/Models/HopBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,11 +10,42 @@ namespace Beernet_Lib.Models
 {
     public class hopbase
     {
-       // [BsonElement("idString")]
+        [BsonElement("idString")]
         public string idString { get; set; }
-      //  [BsonElement("name")]
+        [BsonElement("createdByUserId")]
+        public string createdByUserId { get; set; }
+        [BsonElement("name")]
         public string name { get; set; }
-      //  [BsonElement("aau")]
+        [BsonElement("aau")]
         public double aau { get; set; }
+        [BsonElement("beta")]
+        public double beta { get; set; }
+        [BsonElement("notes")]
+        public string notes { get; set; }
+        [BsonElement("type")]
+        public string type { get; set; }
+
+        //random weird stuff
+        [BsonElement("hsi")]
+        public double hsi { get; set; }
+        [BsonElement("origin")]
+        public string origin { get; set; }
+        [BsonElement("substitues")]
+        public string substitutes { get; set; }
+        [BsonElement("humulene")]
+        public double humulene { get; set; }
+        [BsonElement("caryophyllene")]
+        public double caryophyllene { get; set; }
+        [BsonElement("cohumulone")]
+        public double cohumulone { get; set; }
+        [BsonElement("myrcene")]
+        public double myrcene { get; set; }
+
+        // // [BsonElement("idString")]
+        //  public string idString { get; set; }
+        ////  [BsonElement("name")]
+        //  public string name { get; set; }
+        ////  [BsonElement("aau")]
+        //  public double aau { get; set; }
     }
 }
---
+++ Brewforge/Controllers/AdminController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Beernet_Lib.Models;
+using Beernet_Lib.Tools;
+using Brewforge;
+using Brewforge.Controllers;
+using Brewforge.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BrewForge.Controllers
+{
+
+    public class AdminController : BaseController
+    {
+        private IHostingEnvironment hostingEnvironment;
+
+        public AdminController(IOptions<AppSettings> settings, IHostingEnvironment env) : base(settings)
+        {
+
+        }
+
+        public static int currentSelectedHop { get; set; }
+        public static int currentSelectedFermentable { get; set; }
+        public static int currentSelectedYeast { get; set; }
+        public static int currentSelectedAdjunct { get; set; }
+        public static int currentSelectedStyle { get; set; }
+        public static List<hopbase> hopOptions { get; set; }
+        public static List<fermentable> fermentableOptions { get; set; }
+        public static List<yeast> yeastOptions { get; set; }
+        public static List<adjunct> adjunctOptions { get; set; }
+        public static List<style> styleOptions { get; set; }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            hopOptions = DataAccess.getAllHops(AppSettings.apiLink, AppSettings.apiAuthToken);
+            fermentableOptions = DataAccess.getAllFermentables(AppSettings.apiLink, AppSettings.apiAuthToken);
+            yeastOptions = DataAccess.getAllYeasts(AppSettings.apiLink, AppSettings.apiAuthToken);
+            adjunctOptions = DataAccess.getAllAdjuncts(AppSettings.apiLink, AppSettings.apiAuthToken);
+            styleOptions = DataAccess.getAllStyles(AppSettings.apiLink, AppSettings.apiAuthToken);
+            currentSelectedHop = 0;
+            currentSelectedFermentable = 0;
+            currentSelectedStyle = 0;
+            currentSelectedYeast = 0;
+            currentSelectedAdjunct = 0;
+
+            IngredientManagerViewModel model = new IngredientManagerViewModel();
+            model.hopOptions = hopOptions;
+            model.fermentableOptions = fermentableOptions;
+            model.yeastOptions = yeastOptions;
+            model.adjunctOptions = adjunctOptions;
+            model.styleOptions = styleOptions;
+            model.selectedAdjunctIndex = currentSelectedAdjunct;
+            model.selectedFermentableIndex = currentSelectedFermentable;
+            model.selectedHopIndex = currentSelectedFermentable;
+            model.selectedStyleIndex = currentSelectedStyle;
+            model.selectedYeastIndex = currentSelectedYeast;
+            return View("Views/Admin/Index.cshtml",model);
+        }
+
+        public IActionResult IngredientManager(int selectedHop = -1, IngredientManagerViewModel returnModel = null, hopbase updatedHop = null)
+        {
+            if(hopOptions.Count == 0)
+            {
+                hopOptions = DataAccess.getAllHops(AppSettings.apiLink, AppSettings.apiAuthToken);
+            }
+            if (fermentableOptions.Count == 0)
+            {
+                fermentableOptions = DataAccess.getAllFermentables(AppSettings.apiLink, AppSettings.apiAuthToken);
+
+            }
+            if (yeastOptions.Count == 0)
+            {
+                yeastOptions = DataAccess.getAllYeasts(AppSettings.apiLink, AppSettings.apiAuthToken);
+            }
+            if (adjunctOptions.Count == 0)
+            {
+                adjunctOptions = DataAccess.getAllAdjuncts(AppSettings.apiLink, AppSettings.apiAuthToken);
+            }
+            if (styleOptions.Count == 0)
+            {
+                styleOptions = DataAccess.getAllStyles(AppSettings.apiLink, AppSettings.apiAuthToken);
+            }
+            if(selectedHop != -1)
+            {
+                currentSelectedHop = selectedHop;
+            }
+
+            if(returnModel != null)
+            {
+                if(returnModel.hopOptions != null && returnModel.hopOptions.Count > 0)
+                {
+                    hopOptions[currentSelectedHop] = returnModel.hopOptions[0];
+                }
+            }
+
+                currentSelectedFermentable = 0;
+                currentSelectedStyle = 0;
+                currentSelectedYeast = 0;
+                currentSelectedAdjunct = 0;
+
+                IngredientManagerViewModel model = new IngredientManagerViewModel();
+                model.hopOptions = hopOptions;
+                model.fermentableOptions = fermentableOptions;
+                model.yeastOptions = yeastOptions;
+                model.adjunctOptions = adjunctOptions;
+                model.styleOptions = styleOptions;
+                model.selectedAdjunctIndex = currentSelectedAdjunct;
+                model.selectedFermentableIndex = currentSelectedFermentable;
+                model.selectedHopIndex = currentSelectedHop;
+                model.selectedStyleIndex = currentSelectedStyle;
+                model.selectedYeastIndex = currentSelectedYeast;
+                return View("Views/Admin/Index.cshtml", model);
+        }
+
+    }
+}
