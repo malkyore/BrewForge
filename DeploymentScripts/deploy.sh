60f8410fa225a14a49f61709eb94236f98951007
nick <nickschwartzrox@gmail.com>
2022-01-14 15:14:00 -0600

    Commit final versions of the deployment scripts

--- DeploymentScripts/deploy.sh
+++ DeploymentScripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -t beer@rest.unacceptable.beer -- "sh -c 'sudo docker images -q craftly.beer_blazor'"
+ssh -t beer@rest.unacceptable.beer -- "sh -c 'sudo chmod +x craftlyStaging/BrewForge/DeploymentScripts/server_deploy.sh && ./craftlyStaging/BrewForge/DeploymentScripts/server_deploy.sh'"
--- DeploymentScripts/server_deploy.sh
+++ DeploymentScripts/server_deploy.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-sudo docker images -q craftly.beer_blazor
+#cd to directory
+cd craftlyStaging/BrewForge/
+#find the ID of the docker container
+DOCKERID=$(sudo docker container ls -q -f "ancestor=craftly.beer_blazor")
+#pull a fresh version of the repository
+git fetch --all
+git reset --hard origin/master
+#stop the container
+sudo docker stop $DOCKERID
+#wait a little to make sure its stopped
+sleep 2s
+#run the docker build
+sudo docker build -f ./Craftly.Beer_Blazor/DockerfileServer -t craftly.beer_blazor .
+start the container
+sudo docker run -d -p 1666:80 craftly.beer_blazor
